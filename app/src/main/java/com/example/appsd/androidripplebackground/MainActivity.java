09bd009c390c907a2b0a55af37f67077d21ebb9f
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-01-29 08:57:57 +0600

    Initial commit

---
+++ app/src/main/java/com/example/appsd/androidripplebackground/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.appsd.androidripplebackground;
+
+/*
+
+part one : 1
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.skyfishjy.library.RippleBackground;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
+        ImageView imageView=(ImageView)findViewById(R.id.centerImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rippleBackground.startRippleAnimation();
+
+
+            }
+
+        });
+    }
+}
+*/
+
+
+//part two : 2
+
+        import java.util.ArrayList;
+        import android.animation.Animator;
+        import android.animation.AnimatorSet;
+        import android.animation.ObjectAnimator;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.os.Handler;
+        import android.view.View;
+        import android.view.animation.AccelerateDecelerateInterpolator;
+        import android.widget.ImageView;
+
+        import com.skyfishjy.library.RippleBackground;
+
+
+public class MainActivity extends Activity {
+
+
+    private ImageView foundDevice;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
+
+
+        final Handler handler=new Handler();
+
+
+        foundDevice=(ImageView)findViewById(R.id.centerImage);
+        ImageView button=(ImageView)findViewById(R.id.centerImage);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rippleBackground.startRippleAnimation();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        foundDevice();
+                    }
+                },3000);
+            }
+        });
+    }
+
+
+    private void foundDevice(){
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(400);
+        animatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
+        ArrayList<Animator> animatorList=new ArrayList<Animator>();
+        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(foundDevice, "ScaleX", 0f, 1.2f, 1f);
+        animatorList.add(scaleXAnimator);
+        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(foundDevice, "ScaleY", 0f, 1.2f, 1f);
+        animatorList.add(scaleYAnimator);
+        animatorSet.playTogether(animatorList);
+        foundDevice.setVisibility(View.VISIBLE);
+        animatorSet.start();
+    }
+}
+
